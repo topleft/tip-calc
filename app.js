5eb8f5231ad046be71b745875d7594a621ba00bb
topleft <pete.james.jeffryes@gmail.com>
2015-07-26 12:03:51 -0600

    functioning calc, validates input and accepts/returns floats

--- app.js
+++ app.js
@@ -1,20 +1,48 @@
 // alert("it is working?");
 
-var button = document.getElementsByTagName("button")[0];
-console.log(button);
 
-button.addEventListener("click", function (e) {
-  e.preventDefault();
+function getUserInput(elem, index){
+  var value = document.getElementsByTagName(elem)[index].value;
+  return parseFloat(value);
+}
 
-  // grab form input and store it in total, then convert to number
-  var total = document.getElementsByTagName('input')[0].value;
-  total = parseInt(total);
 
-  // calculate tip
-  var tip = total * 0.2;
-  tip = tip.toFixed(2);
+function calcTip (total){
+    var tip = total * 0.20;
+    tip = tip.toFixed(2);
+    return tip;
+}
 
-  //fill in tip amount
+function evaluateAndPostTip(tip, where) {
+  if (isNaN(tip)) {
+    where.innerHTML = "Please enter a valid dollar amount";
+  }
+  else {
+    where.innerHTML = "You should tip: $"+tip;
+  }
+}
+
+// function addListener(elem, index, action, func) {
+//   var button = document.getElementsByTagName(elem)[index];
+//   button.addEventListener(action, function(e){
+//     e.preventDefault();
+//     return func;
+//   });
+// }
+
+
+function runCalc(){
   var tipSpot = document.getElementById("tip");
-  tipSpot.innerHTML ="$"+tip;
-});
+  var button = document.getElementsByTagName("button")[0];
+
+  button.addEventListener("click", function(e){
+    e.preventDefault();
+    var userInput = getUserInput("input", 0);
+    var tip = calcTip(userInput);
+    evaluateAndPostTip(tip, tipSpot);
+  });
+}
+
+
+runCalc();
+
